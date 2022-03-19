8b5f4f0fe43b0f3037f7cb5c2b413c7b2d0111f2
TranVanDo <dox1dh@gmail.com>
2022-03-19 22:09:53 +0700

    Cập nhật lần 1

--- app/src/main/java/com/example/mad_n5_t16/MainActivity.java
+++ app/src/main/java/com/example/mad_n5_t16/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mad_n5_t16;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnCallActivity1;
+    private Button btnCallActivity2;
+    private Button btnCallActivity3;
+    private Button btnCallActivity4;
+    private Button btnCallActivity5;
+    private Button btnCallActivity6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCallActivity1 = (Button) findViewById(R.id.btnCallActivity1);
+        btnCallActivity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Activity1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
