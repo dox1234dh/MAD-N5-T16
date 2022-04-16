db7f184e9054435ee0543b772d7a653d037166fd
Luckfier <nguyenvietnamhg@gmail.com>
2022-04-16 22:50:02 +0700

    day lai

--- app/src/main/java/com/example/mad_n5_t16/LoginActivity.java
+++ app/src/main/java/com/example/mad_n5_t16/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.mad_n5_t16;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
