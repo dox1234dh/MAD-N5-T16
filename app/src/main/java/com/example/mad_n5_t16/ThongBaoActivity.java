f7838cc6d2aeff2dac61c44a153eae586f39cd56
Shi Ying Nguyen <>
2022-04-24 11:31:30 +0700

    no message

--- app/src/main/java/com/example/mad_n5_t16/ThongBaoActivity.java
+++ app/src/main/java/com/example/mad_n5_t16/ThongBaoActivity.java
@@ -3,10 +3,7 @@ package com.example.mad_n5_t16;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ThongBaoActivity extends AppCompatActivity {
@@ -14,19 +11,9 @@ public class ThongBaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mh_thongbao);
-        TextView titleBar = findViewById(R.id.textView);
+        TextView titleBar = findViewById(R.id.textTitle);
         titleBar.setText("Thông báo");
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        ImageView markerNoti = findViewById(R.id.marker);
-        markerNoti.setImageResource(R.drawable.marker_2);
-        ImageView homeGo = findViewById(R.id.home);
-        homeGo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent goHome = new Intent(ThongBaoActivity.this, MainActivity.class);
-                startActivity(goHome);
-            }
-        });
     }
 }
--- app/src/main/java/com/example/mad_n5_t16/mh_thongtincanhan.java
+++ app/src/main/java/com/example/mad_n5_t16/mh_thongtincanhan.java
@@ -1,16 +1,14 @@
-package com.example.mad_n5_t16.Starter;
+package com.example.mad_n5_t16;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.mad_n5_t16.R;
-
-public class StarterActivity extends AppCompatActivity {
+public class mh_thongtincanhan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_starter);
+        setContentView(R.layout.activity_mh_thongtincanhan);
     }
 }
