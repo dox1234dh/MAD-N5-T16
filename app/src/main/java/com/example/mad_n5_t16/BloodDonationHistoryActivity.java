0256bb92d92321043acf9be4bdff9d1db120f247
Luckfier <nguyenvietnamhg@gmail.com>
2022-04-28 09:54:55 +0700

    them nav cho lich su hien mau, them } cho login

--- app/src/main/java/com/example/mad_n5_t16/BloodDonationHistoryActivity.java
+++ app/src/main/java/com/example/mad_n5_t16/BloodDonationHistoryActivity.java
@@ -3,7 +3,10 @@ package com.example.mad_n5_t16;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +14,7 @@ import android.widget.Toast;
 import com.example.mad_n5_t16.Model.History;
 import com.example.mad_n5_t16.Model.HistoryAdapter;
 import com.example.mad_n5_t16.model_class.DatabaseHelper;
+import com.example.mad_n5_t16.user.MainActivityUser;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,6 +24,7 @@ public class BloodDonationHistoryActivity extends AppCompatActivity {
     HistoryAdapter historyAdapter;
     private History[] histories;
     TextView txtHoVaTen;
+    ImageView marker, home, heart, history, infor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +35,25 @@ public class BloodDonationHistoryActivity extends AppCompatActivity {
         txtHoVaTen = findViewById(R.id.txtHoVaTen);
         txtHoVaTen.setText("Lịch sử hiến máu");
 
+        home = findViewById(R.id.home);
+        home.setImageResource(R.drawable.home_1);
+        marker = findViewById(R.id.marker);
+        marker.setImageResource(R.drawable.marker_1);
+        heart = findViewById(R.id.heart_plus);
+        heart.setImageResource(R.drawable.heart_plus_1);
+        history = findViewById(R.id.order_history);
+        history.setImageResource(R.drawable.order_history_2);
+        infor = findViewById(R.id.guest_male);
+        infor.setImageResource(R.drawable.guest_male_1);
+
 
         ArrayList<History> listHistory = new ArrayList<>();
         DatabaseHelper dbh = new DatabaseHelper(getBaseContext());
 
         //Sau nay lay gia tri tu nguoi dang nhap
-        int idNguoiHienMau = 1;
+        Intent intent=getIntent();
+
+        int idNguoiHienMau = intent.getIntExtra("id",1);
 
         listHistory = dbh.nam_getLichSuHienMauByIdNguoiHienMau(idNguoiHienMau);
         int len = listHistory.size();
@@ -53,5 +71,35 @@ public class BloodDonationHistoryActivity extends AppCompatActivity {
         }else{
             Toast.makeText(this, "Bạn chưa đi hiến máu lần nào", Toast.LENGTH_SHORT).show();
         }
+
+        marker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goMarker = new Intent(BloodDonationHistoryActivity.this, ThongBaoActivity.class);
+                startActivity(goMarker);
+            }
+        });
+        heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goHeart = new Intent(BloodDonationHistoryActivity.this, MainActivity.class);
+                startActivity(goHeart);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goHome = new Intent(BloodDonationHistoryActivity.this, MainActivityUser.class);
+                goHome.putExtra("id", idNguoiHienMau);
+                startActivity(goHome);
+            }
+        });
+        infor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goInfor = new Intent(BloodDonationHistoryActivity.this, MainActivity.class);
+                startActivity(goInfor);
+            }
+        });
     }
 }
